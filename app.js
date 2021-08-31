fdf764b0585569c59d4ffd3afb8b1d018788c72a
himified <emuverma1@gmail.com>
2021-09-01 00:47:38 +0530

    Added graph.ejs

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const ejs = require("ejs")
 const app = express()
 const port = 3000
 
+
 app.set('view engine', 'ejs');
+
+
 
 app.use(express.static("public"));
 app.get('/', (req, res) => {
+    
     res.render("home");
+
 })
-app.post('/Result', (req, res) => {
+
+
+app.post('/Result', (req, res) => {  
     res.render("graph");
+
 })
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
