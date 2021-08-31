7d7fe76ccaabc29cb75b5e4702ba71bb64fd99c4
himified <emuverma1@gmail.com>
2021-08-31 17:25:39 +0530

    Form window

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+const ejs = require("ejs")
+const app = express()
+const port = 3000
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+app.get('/', (req, res) => {
+    res.render("home");
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
